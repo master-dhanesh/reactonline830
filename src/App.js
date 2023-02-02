cffe406a3c3152d391982adb0ca601b6ed425437
master-dhanesh <dhanesh1296@gmail.com>
2023-02-02 21:09:31 +0530

    lifecycle class component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
-import { useState } from "react";
-import Create from "./components/Create";
+import React, { Component } from "react";
 import Show from "./components/Show";
 
-function App() {
-    const [tasks, setTasks] = useState([]);
-    return (
-        <div className="container p-5">
-            <Create tasks={tasks} setTasks={setTasks} />
-            <hr />
-            <Show tasks={tasks} setTasks={setTasks} />
-        </div>
-    );
+class App extends Component {
+    // there is no constructor
+    // state = "asdas";
+
+    constructor(props) {
+        // to init the state data
+        super(props);
+        this.state = {
+            image: [],
+            show: false,
+        };
+        console.log("[app.js] constructor");
+    }
+
+    render() {
+        // to load the view(html/css) without original data
+        // whenever the state changes(data arrives) the render will update/reset/refresh
+        console.log("[app.js] render");
+        return (
+            <div>
+                <h1>This is App.js</h1>
+                {this.state.show ? <Show /> : "Click to show"}
+                <button
+                    onClick={() => this.setState({ show: !this.state.show })}
+                >
+                    Click
+                </button>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        // runs one in a lifetime when the component created
+        // to call the data from backend/ajax and set to the state
+        console.log("[app.js] componentdidmount");
+
+        fetch("https://picsum.photos/v2/list")
+            .then((d) => d.json())
+            .then((data) => {
+                console.log(data);
+                this.setState({ images: data });
+            })
+            .catch((err) => console.log(err));
+    }
+
+    // ---------------------------------------------------------------
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log(prevProps, prevState);
+        console.log("[app.js] componentdidupdate");
+    }
 }
+
 export default App;
--- src/components/Show.js
+++ src/components/Show.js
@@ -1,53 +1,11 @@
-import { useAlert } from "react-alert";
-import "./show.css";
+import React, { Component } from "react";
 
-const Show = (props) => {
-    const alert = useAlert();
-    const { tasks, setTasks } = props;
-    const deletetask = (index) => {
-        const copytasks = [...tasks];
-        copytasks.splice(index, 1);
-        setTasks(copytasks);
-        alert.success("task removed!");
-    };
-
-    let tasklist = (
-        <p className="mt-4 text-center text-success fs-4">task empty!</p>
-    );
-
-    if (tasks.length > 0) {
-        tasklist = tasks.map((task, index) => {
-            return (
-                <li
-                    key={index}
-                    className="list-group-item d-flex justify-content-between align-items-start"
-                >
-                    <div className="ms-2 me-auto">
-                        <div className="fw-bold">{task.title}</div>
-                        {task.description}
-                    </div>
-                    <span
-                        onClick={() => deletetask(index)}
-                        className="px-3 adge bg-danger rounded-pill"
-                    >
-                        <i className="text-white fs-5 ri-delete-bin-7-line"></i>
-                    </span>
-                </li>
-            );
-        });
+export default class Show extends Component {
+    componentWillUnmount() {
+        console.log("[show.js] componentwillunmount");
+        window.confirm("want to leave this page ?");
     }
-
-    const stylep = {
-        fontSize: "2vmax",
-    };
-
-    return (
-        <ol className="list-group list-group-numbered">
-            <h3 style={{ fontSize: "1.5vmax" }}>Task Lists</h3>
-            <p style={stylep}>Lorem ipsum dolor sit amet.</p>
-            {tasklist}
-        </ol>
-    );
-};
-
-export default Show;
+    render() {
+        return <div>This is Show Component.</div>;
+    }
+}
