dbe6ba1fb5a7f03d9e5a6178585ddc2d837b9ef9
master-dhanesh <dhanesh1296@gmail.com>
2023-02-06 21:17:23 +0530

    Gallery pagination

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
+import LazyLoad from "react-lazyload";
 
+import "./App.css";
 const App = () => {
     const [images, setImages] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const getImages = () => {
-        fetch("https://picsum.photos/v2/list")
-            .then((d) => d.json())
-            .then((data) => {
-                console.log(data);
+    const getImages = async (count = 1) => {
+        try {
+            const { data } = await axios.get(
+                `https://picsum.photos/v2/list?page=${count}&limit=13`
+            );
+            setTimeout(() => {
                 setImages(data);
-            })
-            .catch((err) => console.log(err));
+                setLoading(false);
+            }, 2000);
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     useEffect(() => {
         getImages();
     }, []);
 
+    const handlePageClick = (e) => {
+        setLoading(true);
+        setImages(null);
+        getImages(e.selected + 1);
+    };
+
     return (
         <div>
             <button onClick={getImages}>Get Images</button>
-            {images ? JSON.stringify(images) : "loading..."}
+            {loading && <h1>Loading...</h1>}
+            <div className="d-flex flex-wrap container">
+                {images &&
+                    images.map((image, index) => (
+                        <div key={index} className="me-3 mb-3">
+                            <LazyLoad height="10vmax">
+                                <img
+                                    style={{
+                                        height: "10vmax",
+                                        width: "15vmax",
+                                    }}
+                                    src={image.download_url}
+                                    alt={image.download_url}
+                                />
+                            </LazyLoad>
+                            <p>{image.author}</p>
+                        </div>
+                    ))}
+            </div>
+            <ReactPaginate
+                className="pagination"
+                breakLabel="..."
+                nextLabel=">"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}
+                pageCount={10}
+                previousLabel="<"
+            />
         </div>
     );
 };
